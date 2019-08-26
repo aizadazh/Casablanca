cbb72d5e0ed069eaed06e1787d58ad375b1be580
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-08-26 18:42:22 +0600

    updated menu

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -57,60 +57,7 @@ categories.forEach(category => {
       });
     });
   });
-  // Menu
-  
-
-    const anchors = document.querySelectorAll('a[href*="#"]');
-        for (let anchor of anchors) {
-            anchor.addEventListener("click", function(e) {
-            e.preventDefault();
-        
-            const blockID = anchor.getAttribute("href");
-        
-            if (anchor.getAttribute("href") === "#") {
-                document.querySelector("body").scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-                });
-            } else {
-                document.querySelector("" + blockID).scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-                });
-            }
-  });
-}
-    $('.nav-btn').on('click', function(e) {
-        e.preventDefault();
-        $('.main-nav').toggleClass('main-nav-active');
-    })
-
-    $('.nav-list a').click(function() {
-        if ( $('.main-nav').hasClass('main-nav-active')) {
-            $('.main-nav').removeClass('main-nav-active');
-            $('.nav-btn').removeClass('nav-btn-active');
-        }
-    })
-    $('body').click(function(e) {
-        $('.main-nav').removeClass('main-nav-active');
-        $('.nav-btn').removeClass('nav-btn-active');        
-    })
-    $('.main-nav').click(function(e) {
-        e.stopPropagation();
-    });
-    $(document).keydown(function(e) {
-        if (e.keyCode == 27) {
-            $('.main-nav').removeClass('main-nav-active');
-            $('.nav-btn').removeClass('nav-btn-active');
-        }
-    });
-    $('.nav-btn').on('click', function(e) {
-        e.preventDefault;
-        $(this).toggleClass('nav-btn-active');
-    });
-  })
-  
-  
+});
   
   
   
